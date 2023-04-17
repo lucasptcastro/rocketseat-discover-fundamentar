d6a0ada2210fd166fbbca9ded2382e5c795002c4
lucasptcastro <lucasptcastro@gmail.com>
2023-04-17 09:21:32 -0300

    🎉 init: first commit

---
+++ pilotando-com-a-dom/script.js
@@ -0,0 +1,7 @@
+// getElementById
+const elementById = document.getElementById("blog-title");
+console.log(elementById);
+
+// getElementsByClassName
+const elementByClassname = document.getElementsByClassName("my-blog");
+console.log(elementByClassname);
