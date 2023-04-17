3569966195f0cb6b7371d52c6dffcb784192475b
lucasptcastro <lucasptcastro@gmail.com>
2023-04-17 09:38:55 -0300

    ✨ feat: added get element by tag, query and queryAll

--- pilotando-com-a-dom/script.js
+++ pilotando-com-a-dom/script.js
@@ -1,7 +1,19 @@
-// getElementById
+// getElementById (element)
 const elementById = document.getElementById("blog-title");
 console.log(elementById);
 
-// getElementsByClassName
+// getElementsByClassName (HTMLCollection)
 const elementByClassname = document.getElementsByClassName("my-blog");
 console.log(elementByClassname);
+
+// getElementsByTagName (HTMLCollection)
+const elementByTag = document.getElementsByTagName("meta");
+console.log(elementByTag);
+
+// querySelector (element) => seleciona o primeiro elemento através da classe, atributo, id
+const elementByQuery = document.querySelector(".my-blog"); // coleta o primeiro item da classe "my-blog"
+console.log(elementByQuery);
+
+// querySelectorAll (Nodelist = dá pra usar um foreach, já o HTMLCollection não) => seleciona todos os elementos através da classe, atributo, id
+const elementByQueryAll = document.querySelectorAll("[href]"); // coleta todos os itens que possuem o atributo href
+elementByQueryAll.forEach((el) => console.log(`O elemento queryAll: ${el}`));
